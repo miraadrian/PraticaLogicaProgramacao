088ef138884b948a32fafde03c5ef236e209102e
Adrian <mira.adrian@outlook.com>
2025-07-16 13:26:03 -0300

    Colocado outras formas de checar paisagem

--- Ex2/index.js
+++ Ex2/index.js
@@ -1,6 +1,20 @@
 //Escrever uma função chamada ePaisagem que recebe dois argumentos, largura e altura de uma imagem (number).
 //Retornar true se a imagem estiver no modo paisagem.
 
-const ePaisagem = (largura, altura) => largura > altura ? true : false;
+const ePaisagem = (largura, altura) => largura > altura;
 
 console.log(ePaisagem(1280, 720))
+
+/*
+Outras formas:
+const ePaisagem = (largura, altura) => {
+    return largura > altura;
+}
+
+===========================================================
+
+function ePaisagem(largura, altura) {
+    return largura > altura;
+}
+
+*/

b347efecb4ff27458677a5fa1de8b85e7396f546
Adrian <mira.adrian@outlook.com>
2025-07-16 13:15:14 -0300

    Feito de maneira prática com arrow function

--- Ex1/index.js
+++ Ex1/index.js
@@ -1,13 +1,52 @@
 //Função que recebe 2 números e retorna o maior valor entre eles
 
-function comparar(num1, num2){
+
+const max2 = (num1, num2) => num1 > num2 ? num1 : num2;
+
+console.log(max2(10, 5));
+
+
+/*function comparar(num1, num2){
     if(num1 > num2){
         return console.log(`${num1} é maior que ${num2}`);
     }else if (num2 > num1){
         return console.log(`${num2} é maior que ${num1}`);
     }else {
         return console.log('Os dois números são iguais!');
+}
+comparar(25,25);
+}*/
+
+
+/*
+Outra forma de realizar
+
+function max(x, y){
+    if(x > y){
+        return x;
+    } else{
+        return y;
     }
 }
 
-comparar(25,25);
+const maior = max(10, 2);
+console.log(maior);
+
+==================================================
+
+function max(x, y){
+    if(x > y) return x;
+    return y;
+}
+
+function max(x, y){
+    return x > y ? : y;
+}
+
+====================================================
+
+const max = (x, y) => {
+    return x > y ? x: y;
+}
+
+*/
